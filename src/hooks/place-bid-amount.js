437cd1d77e08ba027162c4495ab5fe69e9bd016d
faizalski <52638045+faizalski@users.noreply.github.com>
2019-11-24 13:44:27 +0100

    Update place-bid-amount.js

--- src/hooks/place-bid-amount.js
+++ src/hooks/place-bid-amount.js
@@ -2,7 +2,7 @@
 
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
-/*
+
 const errors = require('feathers-errors');
 
 module.exports = function(options = {}) {
@@ -11,18 +11,9 @@ module.exports = function(options = {}) {
     if (hook.data.update) {
       return hook;
     }
-    if (parseFloat(hook.data.current_price) <= 0) {
-      throw new errors.BadRequest('Invalid Parameters', {
-        errors: { current_price: 'Has to be greater than or equal to 0' }
-      });
-    }
 
 
-    return hook.app.service('auctions').get(hook.id).then(auction => {
-      const price = parseFloat(auction.current_price);
-      hook.data.current_price = price + parseFloat(hook.data.current_price);
-      return hook;
-    });
+
+
   };
 };
-*/
