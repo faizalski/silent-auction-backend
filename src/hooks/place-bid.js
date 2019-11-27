38cc64cb2cd132ab3f70a5d8990ab73cca77b207
faizalski <52638045+faizalski@users.noreply.github.com>
2019-11-27 02:45:33 +0100

    Update place-bid.js

--- src/hooks/place-bid.js
+++ src/hooks/place-bid.js
@@ -26,7 +26,7 @@ module.exports = function(options = {}) {
           hook.data.current_price = price + parseFloat(hook.data.current_price);
 
 
-          var str1 = auction.bidlog + " ; Price:" + price + " - Time:" + Date.now() + "" ;
+          var str1 = auction.bidlog + " ; Price:" + price + " - Time:" + NOW() + "" ;
 
 
           hook.data.bidlog = str1;
@@ -49,3 +49,37 @@ module.exports = function(options = {}) {
 
   };
 };
+
+
+
+function NOW() {
+
+    var date = new Date();
+    var aaaa = date.getFullYear();
+    var gg = date.getDate();
+    var mm = (date.getMonth() + 1);
+
+    if (gg < 10)
+        gg = "0" + gg;
+
+    if (mm < 10)
+        mm = "0" + mm;
+
+    var cur_day = aaaa + "-" + mm + "-" + gg;
+
+    var hours = date.getHours()
+    var minutes = date.getMinutes()
+    var seconds = date.getSeconds();
+
+    if (hours < 10)
+        hours = "0" + hours;
+
+    if (minutes < 10)
+        minutes = "0" + minutes;
+
+    if (seconds < 10)
+        seconds = "0" + seconds;
+
+    return cur_day + " " + hours + ":" + minutes + ":" + seconds;
+
+}
